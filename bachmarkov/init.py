c53c8c6cf4b20822999c3fb54baab48e1d8c9317
Gilbert Wassermann <gilbertwassermann@Gilberts-MacBook-Pro.local>
2017-11-22 13:05:07 -0500

    ENH: initial commit of scaffolding

---
+++ bachmarkov/init.py
@@ -0,0 +1,2 @@
+def markov():
+	print("Hello")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bachmarkov',
+      version='0.0',
+      description="Repo to store Thesis of Gilbert Wassermann Harvard College '18",
+      url='-',
+      author='Gilbert Wassermann',
+      author_email='wassermann@college.harvard.edu',
+      license='MIT',
+      packages=['bachmarkov'],
+      zip_safe=False)
