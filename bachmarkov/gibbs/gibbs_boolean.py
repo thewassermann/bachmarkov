ENH: better constraints loglikelihood fix

bachmarkov/gibbs/gibbs_boolean.py
@@ -166,7 +166,7 @@ class GibbsBooleanSampler():
 		# add in expression
 		soprano = stream.Part(self.soprano)
 		out_stream = stream.Stream()
-		soprano_flat = list(soprano.recurse(classFilter=('Note', 'Rest')))
+		soprano_flat = self.soprano_flat
 		soprano = extract_utils.flattened_to_stream(
 			soprano_flat,
 			self.bassline,
@@ -185,10 +185,10 @@ class GibbsBooleanSampler():
 		bassline = stream.Part(self.bassline)
 		bassline.id = 'Bass'
 
-		s.insert(0, soprano)
-		s.insert(0, alto)
-		s.insert(0, tenor)
-		s.insert(0, bassline)
+		s.insert(0, copy.deepcopy(soprano))
+		s.insert(0, copy.deepcopy(alto))
+		s.insert(0, copy.deepcopy(tenor))
+		s.insert(0, copy.deepcopy(bassline))
 		return s
 
 
@@ -205,18 +205,19 @@ class GibbsBooleanSampler():
 			# profile_df.columns = list(self.constraint_dict.keys())
 			profile_array = np.empty((int(np.floor(n_iter/self.thinning)), ))
 
+		# get length of the chorale
+		len_chorale = len(self.alto_flat)
+
+		# get index of rests 
+		no_rests_idxs = [x for x in np.arange(len_chorale) if not self.alto_flat[x].isRest]
+
+
+		loop_count = 0
 		for i in trange(n_iter, desc='Iteration', disable=self.progress_bar_off):
 		# for i in np.arange(n_iter):
 
-			# get length of the chorale
-			alto_flat = list(self.alto.recurse(classFilter=('Note', 'Rest')))
-			len_chorale = len(alto_flat)
-
-			# get index of rests 
-			no_rests_idxs = [x for x in np.arange(len_chorale) if not alto_flat[x].isRest]
-
 			# choose a random index
-			idx = np.random.choice(no_rests_idxs)
+			idx = no_rests_idxs[i - (loop_count * len(no_rests_idxs))]
 
 			# choose random part
 			if np.random.uniform() < .5:
@@ -226,9 +227,9 @@ class GibbsBooleanSampler():
 
 
 			if target_part_name == 'Alto':
-				self.alto = self.local_search_gibbs(self.alto, 'Alto', idx)
+				self.alto_flat = self.local_search_gibbs(self.alto_flat, 'Alto', idx)
 			else:
-				self.tenor = self.local_search_gibbs(self.tenor, 'Tenor', idx)
+				self.tenor_flat = self.local_search_gibbs(self.tenor_flat, 'Tenor', idx)
 				
 			# run the profiler
 			# if profiling:
@@ -242,6 +243,14 @@ class GibbsBooleanSampler():
 
 			# simulated annealing step
 			self.simulated_annealing(i, self.T_0)
+
+			# decide whether to loop or not
+			if i % len(no_rests_idxs) == len(no_rests_idxs) - 1:
+				loop_count += 1
+
+			# backwards on alternate loops
+			if loop_count % 2 == 1:
+				no_rests_idxs.reverse()
 		
 		if profiling and plotting:
 
@@ -283,11 +292,13 @@ class GibbsBooleanSampler():
 
 			plt.plot(np.arange(int(np.floor(n_iter/self.thinning))) * self.thinning, profile_array)
 
+		self.alto = extract_utils.flattened_to_stream(self.alto_flat, self.bassline, stream.Stream(), 'Alto', self.fermata_layer)
+		self.tenor = extract_utils.flattened_to_stream(self.tenor_flat, self.bassline, stream.Stream(), 'Tenor', self.fermata_layer)
+
 		if profiling:
 			# return profile_df.mean(axis=1)
 			# return the whole df
 			return profile_array # profile_df
-
 
 	def local_search_gibbs(self, part_, part_name, index_):
 		"""
@@ -296,13 +307,25 @@ class GibbsBooleanSampler():
 		Search through the possible notes and select the note that does not satisfy
 		the least number of constraints (a.k.a satisfies the most)
 		"""
+
+		if index_ == 0:
 		
-		possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
-			extract_utils.extract_notes_from_chord(self.chords[index_]),
-			self.key,
-			self.vocal_range_dict[part_name]
-		)
+			possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
+				extract_utils.extract_notes_from_chord(self.chords[index_]),
+				self.key,
+				self.vocal_range_dict[part_name],
+				None
+			)
+
+		else:
 		
+			possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
+				extract_utils.extract_notes_from_chord(self.chords[index_]),
+				self.key,
+				self.vocal_range_dict[part_name],
+				part_[index_ -1]
+			)
+
 		# has possible notes in dict as note.Note is an unhashable type
 		possible_dict = {n.nameWithOctave : n for n in possible_notes}
 
@@ -312,7 +335,11 @@ class GibbsBooleanSampler():
 		for note_ in possible_notes:
 			
 			# create chain including possible note
-			possible_chain = list(part_.recurse(classFilter=('Note', 'Rest')))
+
+			if part_name == 'Alto':
+				possible_chain = self.alto_flat
+			else:
+				possible_chain = self.tenor_flat
 			possible_chain[index_] = note_
 
 			if self.weight_dict is None:
@@ -338,14 +365,14 @@ class GibbsBooleanSampler():
 		chosen_note_name = np.random.choice(list(note_constraints_dict.keys()), p=list(note_probs.values()))
 		chosen_note = note.Note(chosen_note_name)
 
-		new_chain = list(part_.recurse(classFilter=('Note', 'Rest')))
+		if part_name == 'Alto':
+			new_chain = self.alto_flat
+		else:
+			new_chain = self.tenor_flat
+
 		new_chain[index_] = chosen_note
-		return extract_utils.flattened_to_stream(
-			new_chain,
-			self.bassline,
-			stream.Stream(),
-			part_name,
-		)
+
+		return new_chain
 
 
 	def log_likelihood(self):
@@ -353,17 +380,17 @@ class GibbsBooleanSampler():
 		Compute loglikelihood of inner parts
 		"""
 
-		soprano = list(self.soprano.recurse(classFilter=('Note', 'Rest')))
-		flat_alto = list(self.alto.recurse(classFilter=('Note', 'Rest')))
-		flat_tenor = list(self.tenor.recurse(classFilter=('Note', 'Rest')))
-		bass = list(self.bassline.recurse(classFilter=('Note', 'Rest')))
+		soprano = self.soprano_flat
+		alto = self.alto_flat
+		tenor = self.tenor_flat
+		bass = self.bass_flat
 
-		p_is = np.empty((len(flat_tenor * 2),))
+		p_is = np.empty((len(tenor) * 2,))
 		
 		index_cnt = 0
 		part_name = 'Tenor'
 		
-		for j in np.arange(len(flat_tenor * 2)):
+		for j in np.arange(len(tenor) * 2):
 			
 			# index flag and previous part determine which part to operate on next
 			if part_name == 'Alto':
@@ -372,44 +399,77 @@ class GibbsBooleanSampler():
 				part_name = 'Alto'
 			
 			# if rest, rest is only choice
-			if self.chords[index_cnt] == -1 or isinstance(flat_tenor[index_cnt], note.Rest) or isinstance(flat_alto[index_cnt], note.Rest):
+			if self.chords[index_cnt] == -1 or isinstance(tenor[index_cnt], note.Rest) or isinstance(alto[index_cnt], note.Rest):
 				p_is[j] = 1
 				continue
 			
-			# possible notes
-			possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
-				extract_utils.extract_notes_from_chord(self.chords[index_cnt]),
-				self.key,
-				self.vocal_range_dict[part_name]
-			)
+			if index_cnt == 0:
+				# possible notes
+				possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
+					extract_utils.extract_notes_from_chord(self.chords[index_cnt]),
+					self.key,
+					self.vocal_range_dict[part_name],
+					None
+				)
+			else:
+
+				if part_name == 'Alto':
+					# possible notes
+					possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
+						extract_utils.extract_notes_from_chord(self.chords[index_cnt]),
+						self.key,
+						self.vocal_range_dict[part_name],
+						alto[index_cnt-1]
+					)
+				else:
+					possible_notes = chord_utils.random_note_in_chord_and_vocal_range(
+						extract_utils.extract_notes_from_chord(self.chords[index_cnt]),
+						self.key,
+						self.vocal_range_dict[part_name],
+						tenor[index_cnt-1]
+					)
 			
 			# loop through notes in chorale
 			note_probabilities_dict = {}
 			
 			# loop through possible notes alto then tenor 
-			for k in np.arange(len(possible_notes)):
+			for k_ in np.arange(len(possible_notes)):
 				
 				if part_name == 'Alto':
-					possible_chain = flat_alto[:]
-					possible_chain[index_cnt] = possible_notes[k]
-				if part_name == 'Tenor':
-					possible_chain = flat_tenor[:]
-					possible_chain[index_cnt] = possible_notes[k]
+					possible_chain = alto[:]
+				else:
+					possible_chain = tenor[:]
+				possible_chain[index_cnt] = possible_notes[k_]
+
 	
 				# for each note in chorale calculate p_i
+				# constraints_not_met = \
+				# 	[self.constraint_dict[constraint].not_satisfied(self, possible_chain, index_cnt, part_name) * \
+				# 	self.weight_dict[constraint] for constraint in list(self.constraint_dict.keys())]
+
+
+				# for each note in chorale calculate p_i
+				constraints_not_met = {constraint : self.constraint_dict[constraint].not_satisfied(self, possible_chain, index_cnt, part_name) \
+					for constraint in list(self.constraint_dict.keys())}
+
+				# add cross constraints reduces number of 
+				for i_, k in enumerate(list(self.constraint_dict.keys())):
+					for j_, l in enumerate(list(self.constraint_dict.keys())):
+						if i_ < j_:
+							constraints_not_met['{}/{}'.format(k,l)] = constraints_not_met[k] * constraints_not_met[l]
+
 				constraints_not_met = \
-					[self.constraint_dict[constraint].not_satisfied(self, possible_chain, index_cnt, part_name) * \
-					self.weight_dict[constraint] for constraint in list(self.constraint_dict.keys())]
+					[constraints_not_met[cnm] * self.weight_dict[cnm] for cnm in list(constraints_not_met.keys())]
 				
-				note_probabilities_dict[possible_notes[k].nameWithOctave] = np.exp(-np.nansum(constraints_not_met)/self.T)
+				note_probabilities_dict[possible_notes[k_].nameWithOctave] = np.exp(-np.nansum(constraints_not_met)/self.T)
 				total_sum = np.nansum(list(note_probabilities_dict.values()))
 				note_probabilities_dict = {key : note_probabilities_dict[key] / total_sum for key in list(note_probabilities_dict.keys())}
 			
 				
 			if part_name == 'Alto':
-				p_is[j] = note_probabilities_dict.get(flat_alto[index_cnt].nameWithOctave, 0.01)
+				p_is[j] = note_probabilities_dict.get(alto[index_cnt].nameWithOctave, 0.01)
 			else:
-				p_is[j] = note_probabilities_dict.get(flat_tenor[index_cnt].nameWithOctave, 0.01)
+				p_is[j] = note_probabilities_dict.get(tenor[index_cnt].nameWithOctave, 0.01)
 			
 			if j % 2 == 1:
 				index_cnt += 1 
@@ -575,7 +635,7 @@ class StepwiseMotion(GibbsConstraint):
 			note_1 : note.Note
 			note_2 : note.Note
 		"""
-		if abs(interval.notesToChromatic(note_1, note_2).semitones) >= 2:
+		if abs(interval.notesToChromatic(note_1, note_2).semitones) > 2:
 			return 1
 		else:
 			return 0

bachmarkov/mh/mh.py
@@ -75,19 +75,19 @@ class MH():
 		Parameters
 		---------
 
-		    bass_on_beat : music21.part
-		        bassline of a chorale parsed so
-		        only the notes on beats are included
+			bass_on_beat : music21.part
+				bassline of a chorale parsed so
+				only the notes on beats are included
 
-		    vocal_range : tuple of music21.pitch
-		        The highest and lowest note that can
-		        be sung by the vocal part to be filled in
+			vocal_range : tuple of music21.pitch
+				The highest and lowest note that can
+				be sung by the vocal part to be filled in
 
 		Returns
 		-------
 
-		    out_stream : music21.stream
-		        part initialized to random note in the range
+			out_stream : music21.stream
+				part initialized to random note in the range
 
 		"""
 		out_stream = stream.Stream()
@@ -220,7 +220,7 @@ class MH():
 
 			return note.Note(pitch_, quarterLength=1)
 
-    
+	
 	def run(self, n_iter, profiling, plotting=True):
 		"""
 		Function to run the full MH algo
@@ -447,9 +447,9 @@ class ContraryMotion(FitnessFunction):
 		Parameters
 		----------
 
-		    melody : music21.Part
-		    
-		    bass : music21.Part
+			melody : music21.Part
+			
+			bass : music21.Part
 
 		Returns
 		-------
@@ -490,15 +490,15 @@ class ContraryMotion(FitnessFunction):
 					cl = 1
 				concordance_tbl.iloc[rw,cl] += 1
 			elif (dir_p1 < 0) and (dir_p2 > 0):
-			    concordance_tbl.iloc[0, 1] += 1
+				concordance_tbl.iloc[0, 1] += 1
 			elif (dir_p1 > 0) and (dir_p2 < 0):
-			    concordance_tbl.iloc[1, 0] += 1
+				concordance_tbl.iloc[1, 0] += 1
 			elif (dir_p1 < 0) and (dir_p2 < 0):
-			    concordance_tbl.iloc[1, 1] += 1
+				concordance_tbl.iloc[1, 1] += 1
 			elif (dir_p1 > 0) and (dir_p2 > 0):
-			    concordance_tbl.iloc[0, 0] += 1
+				concordance_tbl.iloc[0, 0] += 1
 
-			    
+				
 		return concordance_tbl
 
 
@@ -520,19 +520,19 @@ class ContraryMotion(FitnessFunction):
 		contrary motiuon given a concordance table
 
 		Paper:
-		    http://journals.sagepub.com/doi/10.1177/001316446002000104
+			http://journals.sagepub.com/doi/10.1177/001316446002000104
 
 		Parameters
 		----------
 
-		    concordance_tbl : pandas.DataFrame
-		        concordance table
-		        
+			concordance_tbl : pandas.DataFrame
+				concordance table
+				
 		Returns
 		-------
 
-		    kappa : float
-		        Cohen's Kappa
+			kappa : float
+				Cohen's Kappa
 		"""
 
 		# number of entries total in table
@@ -543,9 +543,9 @@ class ContraryMotion(FitnessFunction):
 
 		# bass up at random
 		p_yes = (concordance_tbl.iloc[0,0] + concordance_tbl.iloc[0,1]) * \
-		    (concordance_tbl.iloc[0,0] + concordance_tbl.iloc[1,0]) / (N**2)
+			(concordance_tbl.iloc[0,0] + concordance_tbl.iloc[1,0]) / (N**2)
 		p_no = (concordance_tbl.iloc[0,1] + concordance_tbl.iloc[1,1]) * \
-		    (concordance_tbl.iloc[1,0] + concordance_tbl.iloc[1,1]) / (N**2)
+			(concordance_tbl.iloc[1,0] + concordance_tbl.iloc[1,1]) / (N**2)
 		random_agreement = p_yes + p_no
 
 		kappa = (contrary - random_agreement) / (1 - random_agreement)
@@ -561,13 +561,13 @@ class ContraryMotion(FitnessFunction):
 		Parameters
 		----------
 
-		    concordance_tbl : pandas.DataFrame
-		        concordance table
-		        
+			concordance_tbl : pandas.DataFrame
+				concordance table
+				
 		Returns
 		-------
 
-		    agreement_prop : float
+			agreement_prop : float
 		"""
 
 		# number of entries total in table
@@ -580,7 +580,7 @@ class ContraryMotion(FitnessFunction):
 		mcnemar_stat = (((concordance_tbl.iloc[1,0] - concordance_tbl.iloc[0,1])**2) / \
 			(concordance_tbl.iloc[1,0] + concordance_tbl.iloc[0,1])) + 0.1
 		return chi2.cdf(mcnemar_stat, df=1)
-    
+	
 	def ff(self, mh, note_, index_):
 		melody = list(mh.melody.recurse(classFilter=('Note', 'Rest')))
 		bass = list(mh.bassline.recurse(classFilter=('Note', 'Rest')))
@@ -589,7 +589,7 @@ class ContraryMotion(FitnessFunction):
 
 		ct = self.concordance_table(melody, bass)
 		return self.metric_func(ct)
-        
+		
 	def profiling(self, mh, bass, melody):
 
 		ct = self.concordance_table(melody, bass)
@@ -599,8 +599,8 @@ class ContraryMotion(FitnessFunction):
 class NotesToTonic(FitnessFunction):
 	"""
 	Function to prefer:
-	    leading note -> tonic
-	    supertonic -> tonic
+		leading note -> tonic
+		supertonic -> tonic
 	"""
 
 	def ff(self, mh, note_, index_):
@@ -621,9 +621,9 @@ class NotesToTonic(FitnessFunction):
 		else:
 			return .001
 
-    
+	
 	def profiling(self, mh, bass, melody):
-	        
+			
 			tonic = mh.key.getTonic().pitchClass
 
 			# convert all to relative pitches and rests
@@ -633,19 +633,19 @@ class NotesToTonic(FitnessFunction):
 					relMelody.append((el.pitch.pitchClass - tonic) % 12)
 				else:
 					relMelody.append(-1)
-	            
-	        
-	        # count total number of possible resolutions
+				
+			
+			# count total number of possible resolutions
 			possible_res_count = relMelody.count(1) + \
 				relMelody.count(2) + \
 				relMelody.count(10) + \
 				relMelody.count(11)
-	            
+				
 			actual_res_count = 0
 			for i in np.arange(1, len(relMelody)):
 				if (relMelody[i-1] in set([1,2,10,11])) and (relMelody[i] == 0):
 					actual_res_count += 1
-	                
+					
 			# extra added in do prevent division by zero
 			return 1 - (actual_res_count / (possible_res_count + 0.001))
 
@@ -672,14 +672,14 @@ class NoIllegalJumps(FitnessFunction):
 					return 0.001
 			else:
 				return 1.
-    
+	
 	def profiling(self, mh, bass, melody):
 
 		no_rests = [x for x in melody if not x.isRest]
 		out_stream = []
 		for i, el in enumerate(no_rests):
 
-            # first entry has no interval
+			# first entry has no interval
 			if i == 0:
 				out_stream.append('0')
 			else:
@@ -695,7 +695,7 @@ class NoConsecutiveIntervals(FitnessFunction):
 
 	To be used with octaves and fifths
 	"""
-    
+	
 	def __init__(self, name, prohibited_interval_list):
 		self.name = name
 		self.prohibited_interval_set = set(prohibited_interval_list)
@@ -713,7 +713,7 @@ class NoConsecutiveIntervals(FitnessFunction):
 			melody_intervals = [i % 12 for i in extract_utils.get_intervals(melody, index_ - 2, index_)]
 			bass_intervals = [i % 12 for i in extract_utils.get_intervals(bass, index_ - 2, index_)]
 
-            # if intervals between intervals are identical
+			# if intervals between intervals are identical
 			between_intervals = (np.array(melody_intervals) - np.array(bass_intervals))
 			if len(between_intervals) == 1:
 				return 1.
@@ -746,11 +746,11 @@ class NoConsecutiveIntervals(FitnessFunction):
 
 
 class JumpThenStep(FitnessFunction):
-    
+	
 	def ff(self, mh, note_, index_):
 
 		melody = list(mh.melody.recurse(classFilter=('Note', 'Rest')))
-        
+		
 		if index_ == len(melody) - 1:
 			s = -3
 			e = -1
@@ -768,7 +768,7 @@ class JumpThenStep(FitnessFunction):
 
 			# direction of jump
 			jump_dir = np.sign(intervals[0])
-            
+			
 			if (intervals[1] <= 2) and \
 				((np.sign(intervals[1]) == (jump_dir * -1)) or (np.sign(intervals[1]) == 0)):
 				return 1.
@@ -776,7 +776,7 @@ class JumpThenStep(FitnessFunction):
 				return 0.001
 		else:
 			return 1.
-        
+		
 	def profiling(self, mh, bass, melody):
 
 		# replace original melody note with proposed note
@@ -786,8 +786,8 @@ class JumpThenStep(FitnessFunction):
 		jump_count = 0
 		step_after_jump_count = 0
 		for i in np.arange(len(intervals) - 1):
-            
-            # identify jump
+			
+			# identify jump
 			if (intervals[i] > 4):
 				jump_count += 1
 				jump_dir = np.sign(intervals[i])
@@ -795,9 +795,106 @@ class JumpThenStep(FitnessFunction):
 				if (intervals[i + 1] <= 2) and \
 					((np.sign(intervals[i + 1]) == (jump_dir * -1)) or (np.sign(intervals[1]) == 0)):
 					step_after_jump_count += 1
-		            
+					
 		return 1 - (step_after_jump_count / (jump_count + 0.001))
-        
+
+
+class StepwiseMotion(Constraint):
+	"""
+	Return 1 if the interval between successive notes is not a second
+	"""
+
+	def not_satisfied(self, MCMC, chain, index_):
+
+		proposed_note = chain[index_]
+
+
+		# if at first note -> just interval after
+		if index_ == 0:
+
+			next_note = chain[index_ + 1]
+
+			# if next note is not a rest
+			if not isinstance(next_note, (note.Rest)) and \
+				(self.is_step(proposed_note, next_note) == 1):
+				return 1
+			else:
+				return 0
+
+		else:
+			previous_note = chain[index_ - 1]
+
+			if not isinstance(previous_note, (note.Rest)) and \
+				(self.is_step(previous_note, proposed_note) == 1):
+				return 1
+			else:
+				return 0
+
+
+	def is_step(self, note_1, note_2):
+		"""
+		Function that returns 1 if `note_1` and `note_2` is within a step
+
+		Parameters
+		----------
+
+			note_1 : note.Note
+			note_2 : note.Note
+		"""
+		if abs(interval.notesToChromatic(note_1, note_2).semitones) > 2:
+			return 1
+		else:
+			return 0
+
+		
+class FollowDirection(Constraint):
+	"""
+	Return 1 if the music changes direction
+	"""
+
+	def not_satisfied(self, MCMC, chain, index_):
+
+		proposed_note = chain[index_]
+
+
+		# if at first note -> just interval after
+		if index_ == 0:
+			return 0
+		elif index_ == len(chain) -1 :
+			return 0
+		else:
+			# get notes needed
+			next_note = chain[index_ + 1]
+			previous_note = chain[index_ - 1]
+			
+
+			notes = [next_note, previous_note, proposed_note]
+			
+			# assuming neither 
+			if not any([isinstance(n, note.Rest) for n in notes]) and \
+				(self.does_not_follow(previous_note, proposed_note, next_note) == 1):
+				return 1
+			else:
+				return 0
+
+
+	def does_not_follow(self, note_1, note_2, note_3):
+		"""
+		Function that returns 1 if the direction of the interval between note_1 and
+		note_2 is different than that between note_2 and note_3
+
+		Parameters
+		----------
+
+			note_1 : note.Note
+			note_2 : note.Note
+		"""
+		if ((note_1.pitch < note_2.pitch) and (note_2.pitch < note_3.pitch)) or \
+			((note_1.pitch > note_2.pitch) and (note_2.pitch > note_3.pitch)):
+				return 0
+		else:
+			return 1
+		
 
 ##### IMPLEMENTATIONS
 

bachmarkov/mh/mh_boolean.py
@@ -149,7 +149,7 @@ class MCMCBooleanSampler():
 		"""
 		
 		# get length of the chorale
-		flat_chorale = list(self.soprano.recurse(classFilter=('Note', 'Rest')))
+		flat_chorale = self.melody
 		len_chorale = len(flat_chorale)
 		
 		# get index of rests 
@@ -173,7 +173,7 @@ class MCMCBooleanSampler():
 			
 			# choose whether to local search or metropolis
 			if np.random.uniform() <= self.ps:
-				self.soprano = self.local_search(self.soprano, idx)
+				self.melody = self.local_search(self.melody, idx)
 
 			else:
 
@@ -196,7 +196,7 @@ class MCMCBooleanSampler():
 					)
 
 				proposed_note = np.random.choice(possible_notes)
-				self.soprano = self.metropolis_move(self.soprano, proposed_note, idx)
+				self.melody = self.metropolis_move(self.melody, proposed_note, idx)
 				
 				
 			# run the profiler
@@ -271,6 +271,8 @@ class MCMCBooleanSampler():
 
 			plt.plot(np.arange(int(np.floor(n_iter/self.thinning))) * self.thinning, profile_array)
 
+		self.soprano = extract_utils.flattened_to_stream(self.melody, self.bassline, stream.Stream(), 'Soprano', fermata_layer=self.fermata_layer)
+
 		if profiling:
 			# return profile_df.mean(axis=1)
 			# return the whole df
@@ -298,8 +300,8 @@ class MCMCBooleanSampler():
 			self.fermata_layer
 		)
 
-		s.insert(0, stream.Part(melody))
-		s.insert(0, stream.Part(bass))
+		s.insert(0, copy.deepcopy(stream.Part(melody)))
+		s.insert(0, copy.deepcopy(stream.Part(bass)))
 		s.show(show_command)
 
 
@@ -313,8 +315,8 @@ class MCMCBooleanSampler():
 		Calculate the loglikelihood of the chain for each iteration
 		"""
 
-		soprano = list(self.soprano.recurse(classFilter=('Note', 'Rest')))
-		bass = list(self.bassline.recurse(classFilter=('Note', 'Rest')))
+		soprano = self.melody
+		bass = self.bass
 
 		p_is = np.empty((len(bass), ))
 
@@ -385,7 +387,7 @@ class MCMCBooleanSampler():
 		"""
 		
 		# turn part_ into a list
-		current_chain = list(part_.recurse(classFilter=('Note', 'Rest')))
+		current_chain = part_
 		
 		# create the proposal chain
 		proposed_chain = current_chain[:]
@@ -418,12 +420,7 @@ class MCMCBooleanSampler():
 		
 		# accept with probability p
 		if np.random.uniform() <= p:
-			return extract_utils.flattened_to_stream(
-				proposed_chain,
-				self.bassline,
-				stream.Stream(),
-				'Soprano'
-			)
+			return proposed_chain
 		else:
 			return part_
 
@@ -441,7 +438,7 @@ class MCMCBooleanSampler():
 		the least number of constraints (a.k.a satisfies the most)
 		"""
 
-		possible_chain = list(part_.recurse(classFilter=('Note', 'Rest')))
+		possible_chain = part_
 
 		if index_ == 0:
 		
@@ -483,19 +480,17 @@ class MCMCBooleanSampler():
 			note_constraints_dict[note_.nameWithOctave] = np.nansum(constraints_not_met)
 			
 		# get best possible note from possible notes
-		best_note = possible_dict[min(note_constraints_dict, key=note_constraints_dict.get)]
+		if len(note_constraints_dict) == 0:
+			best_note = part[index_]
+		else:
+			best_note = possible_dict[min(note_constraints_dict, key=note_constraints_dict.get)]
 		
 		# if a tie between best notes
 		# TODO
 		
-		new_chain = list(part_.recurse(classFilter=('Note', 'Rest')))
+		new_chain = part_
 		new_chain[index_] = best_note
-		return extract_utils.flattened_to_stream(
-			new_chain,
-			self.bassline,
-			stream.Stream(),
-			'Soprano'
-		)
+		return new_chain
 
 
 	def simulated_annealing(self, iter_, T_0):
@@ -1070,7 +1065,6 @@ class MovementWithinThird(Constraint):
 		else:
 			return 0
 
-
 class CrossConstraint(Constraint):
 
 	def __init__(self, name, constraint_list):
@@ -1082,6 +1076,103 @@ class CrossConstraint(Constraint):
 
 	def profiling(self, MCMC, line):
 		return np.nanprod([c.profiling(MCMC, line) for c in self.cl])
+
+
+class StepwiseMotion(Constraint):
+	"""
+	Return 1 if the interval between successive notes is not a second
+	"""
+
+	def not_satisfied(self, MCMC, chain, index_):
+
+		proposed_note = chain[index_]
+
+
+		# if at first note -> just interval after
+		if index_ == 0:
+
+			next_note = chain[index_ + 1]
+
+			# if next note is not a rest
+			if not isinstance(next_note, (note.Rest)) and \
+				(self.is_step(proposed_note, next_note) == 1):
+				return 1
+			else:
+				return 0
+
+		else:
+			previous_note = chain[index_ - 1]
+
+			if not isinstance(previous_note, (note.Rest)) and \
+				(self.is_step(previous_note, proposed_note) == 1):
+				return 1
+			else:
+				return 0
+
+
+	def is_step(self, note_1, note_2):
+		"""
+		Function that returns 1 if `note_1` and `note_2` is within a step
+
+		Parameters
+		----------
+
+			note_1 : note.Note
+			note_2 : note.Note
+		"""
+		if abs(interval.notesToChromatic(note_1, note_2).semitones) >= 2:
+			return 1
+		else:
+			return 0
+
+		
+class FollowDirection(Constraint):
+	"""
+	Return 1 if the music changes direction
+	"""
+
+	def not_satisfied(self, MCMC, chain, index_):
+
+		proposed_note = chain[index_]
+
+
+		# if at first note -> just interval after
+		if index_ == 0:
+			return 0
+		elif index_ == len(chain) -1 :
+			return 0
+		else:
+			# get notes needed
+			next_note = chain[index_ + 1]
+			previous_note = chain[index_ - 1]
+			
+
+			notes = [next_note, previous_note, proposed_note]
+			
+			# assuming neither 
+			if not any([isinstance(n, note.Rest) for n in notes]) and \
+				(self.does_not_follow(previous_note, proposed_note, next_note) == 1):
+				return 1
+			else:
+				return 0
+
+
+	def does_not_follow(self, note_1, note_2, note_3):
+		"""
+		Function that returns 1 if the direction of the interval between note_1 and
+		note_2 is different than that between note_2 and note_3
+
+		Parameters
+		----------
+
+			note_1 : note.Note
+			note_2 : note.Note
+		"""
+		if ((note_1.pitch < note_2.pitch) and (note_2.pitch < note_3.pitch)) or \
+			((note_1.pitch > note_2.pitch) and (note_2.pitch > note_3.pitch)):
+				return 0
+		else:
+			return 1
 
 
 def create_cross_constraint_dict(constraint_dict, model):
@@ -1112,36 +1203,6 @@ def create_cross_constraint_dict(constraint_dict, model):
 			
 	return cd_out  
 
-# trained weights
-# NIJ          6.720067
-# NIJ/NPI      6.602889
-# NIJ/CM       7.351684
-# NIJ/NTT      6.749566
-# NIJ/LTS      6.011395
-# NIJ/RR       7.656894
-# NIJ/MWT      6.948376
-# NPI          6.044982
-# NPI/CM       6.367033
-# NPI/NTT      7.165307
-# NPI/LTS      6.124189
-# NPI/RR       6.930095
-# NPI/MWT      5.977234
-# CM           3.956699
-# CM/NTT       7.115753
-# CM/LTS       5.530217
-# CM/RR        7.030064
-# CM/MWT       6.264896
-# NTT          6.689717
-# NTT/LTS      6.716246
-# NTT/RR       6.208140
-# NTT/MWT      6.814695
-# LTS          3.973700
-# LTS/RR       7.024067
-# LTS/MWT      3.802352
-# RR           5.215452
-# RR/MWT       5.803847
-# MWT          3.683069
-# MSE        948.166726
 
 
 		

bachmarkov/tuning/regularization.py
@@ -185,9 +185,7 @@ class MCMCMinimizer(mh_boolean.MCMCBooleanSampler):
 		
 		wd_, wd_cross = self.split_dicts(weight_dict)
 
-		return -np.nansum(np.log(p_is)) + \
-			(lambda_1 * np.nansum(np.array(list(wd_.values()))**2)) + \
-			(lambda_2 * np.nansum(np.array(list(wd_cross.values()))**2))
+		return -np.nansum(np.log(p_is))
 	
 	
 	
@@ -561,9 +559,7 @@ class GibbsMinimizer(gibbs_boolean.GibbsBooleanSampler):
 		wd_, wd_cross = self.split_dicts(weight_dict)
 
 		# return scores
-		return -np.nansum(np.log(p_is)) + \
-			(lambda_1 * np.nansum(np.array(list(wd_.values()))**2)) + \
-			(lambda_2 * np.nansum(np.array(list(wd_cross.values()))**2))
+		return -np.nansum(np.log(p_is))
 
 def optimize_chorales_gibbs(chorale, cd_gibbs, lambda_1, lambda_2):
 